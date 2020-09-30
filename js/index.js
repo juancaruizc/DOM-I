46642a98e0cc45f76db03fcbbf0d9e458cef8b2f
juancaruizc <juancruizcruz@gmail.com>
2020-09-29 20:55:27 -0700

    task1

--- js/index.js
+++ js/index.js
@@ -41,4 +41,29 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-// test
+
+// Create selectors to point your data into elements
+//  Create selectors by using any of the DOM element's methods
+//  Note that IDs have been used on all images. Use the IDs to update src path content
+
+const newImg1 = document.querySelector ('#logo-img')
+newImg1.src = 'https://i.ytimg.com/vi/h5TpJb_jE-M/maxresdefault.jpg'
+
+const newImg2 = document.querySelector ('#cta-img')
+newImg2.src = 'https://i.ytimg.com/vi/h5TpJb_jE-M/maxresdefault.jpg'
+
+const newImg3 = document.querySelector ('#middle-img')
+newImg3.src = 'https://i.ytimg.com/vi/h5TpJb_jE-M/maxresdefault.jpg'
+
+// Update the HTML with the JSON data
+//  Remember, NO direct updating of the HTML source is allowed.
+//  Using your selectors, update the content to match the example file.
+//  Remember to update the src attributes on images
+
+
+
+
+// Add new content
+//  Change the color of the navigation text to be green.
+//  Utilize .appendChild() and .prepend() to add two new items to the navigation system. You can call them whatever you want.
+//  Check your work by looking at the original html in the browser
